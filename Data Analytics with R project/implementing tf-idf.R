56cdb9a71c7d5077a2140ee5f017548adf817b7e
Neeraj Subhedar <Neeraj Subhedar>
2017-04-14 22:47:43 -0400

    commit

--- Data Analytics with R project/implementing tf-idf.R
+++ Data Analytics with R project/implementing tf-idf.R
@@ -1,6 +1,4 @@
-generateWordCloud.tmTfIdf <- function(object.with.tweets, minimum.frequency = 10){
-  
-  ## Cleans the tweets ans stores them as a dataframe
+## Cleans the tweets ans stores them as a dataframe
   #df.tweets <- cleanTweets(object.with.tweets)
   df.tweets <- cleanTweets(object.with.tweets)
   # Creates a text corpus from the plain text document for every tweet
@@ -11,6 +9,8 @@ generateWordCloud.tmTfIdf <- function(object.with.tweets, minimum.frequency = 10
   tdm <- DocumentTermMatrix(text_corpus, control = list(weighting = weightTfIdf))
   tdm <- removeSparseTerms(tdm,sparse = 0.95)
   
+  Zipf_plot(tdm,type = "l",col="blue")
+  # the line
   inspect(tdm)
   
   # converting term document matrix to matrix
@@ -21,11 +21,19 @@ generateWordCloud.tmTfIdf <- function(object.with.tweets, minimum.frequency = 10
   word_freqs <- sort(colSums(m), decreasing = TRUE)
   # create a data frame with words and their frequencies
   dm <- data.frame(word = names(word_freqs), freq = word_freqs)
-  
+  plot(dm$freq,type = "l")
+  plot(density(dm$freq),type = "l")
+  abline(v = mean(dm$freq), col = "red")
+  #abline(v = median(dm$freq), col = "blue")
+  #abline(v = mean(dm$freq) - sd(dm$freq), col = "green")
+  #abline(v = mean(dm$freq) + sd(dm$freq), col = "green")
+  abline(v = mean(dm$freq) - 2*sd(dm$freq), col = "blue")
+  abline(v = mean(dm$freq) + 2*sd(dm$freq), col = "blue")
+  subset.dm <- dm[dm$freq<=mean(dm$freq) + 2*sd(dm$freq) & dm$freq>=mean(dm$freq) - 2*sd(dm$freq),]
+  plot(subset.dm$freq, type = "l")
   # creating word cloud
   # wordcloud(word, associated frequency, ordering, color palette)
   #wordcloud(dm$word, dm$freq, min.freq=minimum.frequency, random.order = FALSE, 
   #          colors = brewer.pal(11, "Spectral"))
-  wordcloud2(data = dm,minSize = 5)
+  wordcloud2(data = subset.dm)
   #png(filename=plotfile1, width=740, height=740, units="px") # optional
-}
